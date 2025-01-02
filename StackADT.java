67e19882f9f14206628578e452ccafc73b0e58b7
Matt M <146141224+Matt1092@users.noreply.github.com>
2025-01-01 22:52:03 -0500

    Update StackADT.java

--- StackADT.java
+++ StackADT.java
@@ -1,3 +1,15 @@
+/**
+ * StackADT.java
+ *
+ * This interface defines the abstract data type for a stack. A stack is a collection that follows the Last-In-First-Out (LIFO) principle.
+ * This interface provides standard stack operations such as push, pop, peek, isEmpty, size, clear, and toString.
+ *
+ * @param <T> the type of elements in this stack
+ * 
+ * Created by: Matthew Moga
+ * Date: January 2, 2025
+ */
+
 public interface StackADT<T> {
     // Adds one element to the top of the stack.
     public void push(T element);
