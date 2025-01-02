d5721ff22117ceac374cb0dc4f020456583763ff
Matt M <146141224+Matt1092@users.noreply.github.com>
2025-01-01 23:06:58 -0500

    Update ListNode.java

--- ListNode.java
+++ ListNode.java
@@ -1,3 +1,16 @@
+/**
+ * ListNode.java
+ *
+ * This class represents a node in a singly linked list. Each node contains data and a reference to the next node in the list.
+ * It provides constructors to create a node with or without a reference to the next node, as well as accessor and mutator methods
+ * to get and set the node's data and the reference to the next node.
+ *
+ * @param <T> the type of elements in this node
+ * 
+ * Created by: Matthew Moga
+ * Date: January 2, 2025
+ */
+
 public class ListNode<T> {
     private T data;
     private ListNode next;
