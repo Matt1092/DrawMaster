9fdbcc0d85043f6ac48b43fa0ff11d7a162b137d
Matt M <146141224+Matt1092@users.noreply.github.com>
2025-01-01 23:05:57 -0500

    Update LinkedListADT.java

--- LinkedListADT.java
+++ LinkedListADT.java
@@ -1,3 +1,19 @@
+/**
+ * LinkedListADT.java
+ *
+ * This interface defines the abstract data type for a singly linked list.
+ * It specifies the standard operations that can be performed on a linked list,
+ * such as adding elements to the front, removing elements from the front, 
+ * checking if the list is empty, clearing the list, checking if the list 
+ * contains a certain element, getting the size of the list, and converting 
+ * the list to a string representation.
+ *
+ * @param <T> the type of elements in this linked list
+ * 
+ * Created by: Matthew Moga
+ * Date: January 2, 2025
+ */
+
 public interface LinkedListADT<T> {
     
     // Returns true if the linked list has no nodes, or false otherwise.
